5c23b495bd3fdc0578305d8ce55716590b8095ab
Santiago <48573143+RovleZMex@users.noreply.github.com>
2022-04-27 18:19:51 -0700

    Agregamos el codigo del dia 7

---
+++ codeDays/day7.py
@@ -0,0 +1,3 @@
+#Declara una variable de tipo cadena con saltos de linea e imprimela
+phrase = "Hi Samuel! \nHow are you doing?"
+print(phrase)
--- day8.py
+++
@@ -1,3 +0,0 @@
-##Declara una lista de numeros e imprime el numero de elementos
-numeros = [3,4,2,2,54]
-print(f"{len(numeros)}")
