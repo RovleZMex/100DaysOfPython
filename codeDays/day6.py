1ba637c2c52debea42e43cedf57b286fd40200b0
Santiago <48573143+RovleZMex@users.noreply.github.com>
2022-04-27 21:26:25 -0700

    agregamos el dia 6

---
+++ codeDays/day6.py
@@ -0,0 +1,7 @@
+#Declara dos variables de tipo cadena, concatenalas e imprimelas
+from operator import concat
+
+phrase1 = "Un tomate es color: "
+phrase2 = "Rojo"
+concatPhrase = concat(phrase1,phrase2)
+print(concatPhrase)
