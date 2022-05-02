8627a63255ba90b676537e93f5791652e7e8f172
Santiago <48573143+RovleZMex@users.noreply.github.com>
2022-05-02 10:14:51 -0700

    Agregamos el codigo del Dia 13

---
+++ codeDays/day13.py
@@ -0,0 +1,5 @@
+#Declara dos variables booleanas, evalua una expresion lógica usando dos operadores lógicos
+#e imprime el resultado
+var1 = True
+var2 = False
+print(var1 == True and var2 == False)
